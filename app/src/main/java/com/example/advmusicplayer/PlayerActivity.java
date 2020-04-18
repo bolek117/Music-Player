f0db42ad4618155e794a14c9f0ecd66e9184f857
bolek117 <bolek117a@gmail.com>
2020-04-18 22:47:24 +0200

    Media looping

--- app/src/main/java/com/example/advmusicplayer/PlayerActivity.java
+++ app/src/main/java/com/example/advmusicplayer/PlayerActivity.java
@@ -1,16 +1,20 @@
 package com.example.advmusicplayer;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -18,7 +22,6 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class PlayerActivity extends AppCompatActivity {
-
     Button btn_next, btn_previous, btn_pause;
     TextView songTextLabel;
     SeekBar songSeekBar;
@@ -28,46 +31,46 @@ public class PlayerActivity extends AppCompatActivity {
     String sname;
 
     ArrayList<File> mySongs;
-    Thread updateseekBar;
+    Thread updateSeekBar;
 
+    final Integer defaultReplayCount = 10;
+
+    TextView etReplayCount;
+    Integer replayCount = defaultReplayCount;
+
+    TextView etRemainingCount;
+    Integer remainingCount = defaultReplayCount;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
-        btn_next = (Button)findViewById(R.id.next);
-        btn_previous = (Button)findViewById(R.id.previous);
-        btn_pause = (Button)findViewById(R.id.pause);
+        btn_next = findViewById(R.id.next);
+        btn_previous = findViewById(R.id.previous);
+        btn_pause = findViewById(R.id.pause);
 
-        songTextLabel = (TextView) findViewById(R.id.songLabel);
+        songTextLabel = findViewById(R.id.songLabel);
+        songSeekBar = findViewById(R.id.seekBar);
 
-        songSeekBar = (SeekBar) findViewById(R.id.seekBar);
+        etReplayCount = findViewById(R.id.etReplayCount);
+        etReplayCount.setText(replayCount.toString());
 
-        getSupportActionBar().setTitle("Now Playing");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        etRemainingCount = findViewById(R.id.etReplayRemaining);
+        etRemainingCount.setText(remainingCount.toString());
 
-        updateseekBar = new Thread()
-        {
-            @Override
-            public void run() {
-                int totalDuration = myMediaPlayer.getDuration();
-                int currentPostion = 0;
 
-                while(currentPostion < totalDuration)
-                {
-                    try {
-                        sleep(500);
-                        currentPostion = myMediaPlayer.getCurrentPosition();
-                        songSeekBar.setProgress(currentPostion);
-                    }
-                    catch (InterruptedException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar == null) {
+            return;
+        }
+
+        supportActionBar.setTitle("Now Playing");
+        supportActionBar.setDisplayHomeAsUpEnabled(true);
+        supportActionBar.setDisplayShowHomeEnabled(true);
+
+        updateSeekBar = createUpdateSeekBar();
 
         if(myMediaPlayer != null)
         {
@@ -80,7 +83,7 @@ public class PlayerActivity extends AppCompatActivity {
 
         mySongs = (ArrayList) bundle.getParcelableArrayList("songs");
 
-        sname = mySongs.get(position).getName().toString();
+        sname = mySongs.get(position).getName();
 
         String songName = i.getStringExtra("songname");
 
@@ -95,7 +98,7 @@ public class PlayerActivity extends AppCompatActivity {
         myMediaPlayer.start();
         songSeekBar.setMax(myMediaPlayer.getDuration());
 
-        updateseekBar.start();
+        updateSeekBar.start();
 
         songSeekBar.getProgressDrawable().setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY);
         songSeekBar.getThumb().setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
@@ -116,7 +119,6 @@ public class PlayerActivity extends AppCompatActivity {
                 myMediaPlayer.seekTo(seekBar.getProgress());
             }
         });
-
 
         btn_pause.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -139,47 +141,153 @@ public class PlayerActivity extends AppCompatActivity {
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myMediaPlayer.stop();
-                myMediaPlayer.release();
-                position = ((position + 1) % mySongs.size());
-
-                Uri u = Uri.parse(mySongs.get(position).toString());
-
-                myMediaPlayer = MediaPlayer.create(getApplicationContext(), u);
-
-                sname = mySongs.get(position).getName().toString();
-                songTextLabel.setText(sname);
-
-                myMediaPlayer.start();
+                nextSong();
             }
         });
 
         btn_previous.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myMediaPlayer.stop();
-                myMediaPlayer.release();
-                position = ((position - 1) < 0) ? (mySongs.size() - 1) : (position - 1);
+                prevSong();
 
-                Uri u = Uri.parse(mySongs.get(position).toString());
-                myMediaPlayer = MediaPlayer.create(getApplicationContext(), u);
+            }
+        });
 
-                sname = mySongs.get(position).getName().toString();
-                songTextLabel.setText(sname);
+        etReplayCount.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) return;
 
-                myMediaPlayer.start();
+                EditText input = (EditText)v;
+                Integer parsedValue = ParseIntegerInput(input);
 
+                if (!parsedValue.equals(replayCount)) {
+                    etReplayCount.setText(parsedValue.toString());
+                    etRemainingCount.setText(parsedValue.toString());
+
+                    replayCount = parsedValue;
+                    updateRemainingCount(parsedValue);
+                }
             }
         });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    private Thread createUpdateSeekBar()
+    {
+        return new Thread()
+        {
+            @Override
+            public void run() {
+                int totalDuration = myMediaPlayer.getDuration();
+                int currentpostion = 0;
 
+                while(currentpostion < totalDuration)
+                {
+                    try {
+                        sleep(500);
+                        currentpostion = myMediaPlayer.getCurrentPosition();
+                        songSeekBar.setProgress(currentpostion);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+    }
+
+    private void prevSong()
+    {
+        updateSeekBar.interrupt();
+        while(!updateSeekBar.isInterrupted()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        myMediaPlayer.stop();
+        myMediaPlayer.release();
+        position = ((position - 1) < 0) ? (mySongs.size() - 1) : (position - 1);
+
+        Uri u = Uri.parse(mySongs.get(position).toString());
+        myMediaPlayer = MediaPlayer.create(getApplicationContext(), u);
+
+        sname = mySongs.get(position).getName();
+        songTextLabel.setText(sname);
+
+        myMediaPlayer.start();
+        setListeners();
+
+        updateSeekBar = createUpdateSeekBar();
+        updateSeekBar.start();
+    }
+
+    private void nextSong()
+    {
+        updateSeekBar.interrupt();
+
+        myMediaPlayer.stop();
+        myMediaPlayer.release();
+        position = ((position + 1) % mySongs.size());
+
+        Uri u = Uri.parse(mySongs.get(position).toString());
+
+        myMediaPlayer = MediaPlayer.create(getApplicationContext(), u);
+
+        sname = mySongs.get(position).getName();
+        songTextLabel.setText(sname);
+
+        myMediaPlayer.start();
+        setListeners();
+
+        updateSeekBar = createUpdateSeekBar();
+        updateSeekBar.start();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
         if(item.getItemId() == android.R.id.home)
         {
             onBackPressed();
         }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private int ParseIntegerInput(EditText et) {
+        Editable value = et.getText();
+        try {
+            int parsed = Integer.parseInt(value.toString());
+            return (parsed < 1 || parsed > 100) ? defaultReplayCount : parsed;
+        } catch (NumberFormatException e) {
+            return defaultReplayCount;
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void updateRemainingCount(int count) {
+        remainingCount = count;
+        etRemainingCount.setText(remainingCount.toString());
+    }
+
+    private void setListeners() {
+        myMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                if (remainingCount-- > 0) {
+                    updateRemainingCount(remainingCount);
+                    mp.seekTo(1);
+                    mp.start();
+                } else {
+                    remainingCount = replayCount;
+                    updateRemainingCount(remainingCount);
+                    nextSong();
+                }
+            }
+        });
     }
 }
